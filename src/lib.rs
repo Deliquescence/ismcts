6365892ec3989720b6f005e1a2bacba374f87c85
Josh Baird <jjb.pub01@gmail.com>
2019-10-27 22:29:08 -0400

    Prototyping

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,52 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::rc::Rc;
+
+pub trait Game: Clone {
+    type Move: Clone;
+    type Player: Clone;
+    type MoveList: std::iter::IntoIterator<Item = Self::Move>;
+
+    fn randomize_determination(&mut self, observer: &Self::Player);
+
+    fn current_player(&self) -> &Self::Player;
+
+    fn next_player(&self) -> &Self::Player;
+
+    fn available_moves(&self) -> Self::MoveList;
+
+    fn make_move(&mut self, mov: &Self::Move);
+
+    fn result(&self, player: &Self::Player) -> Option<f64>;
+}
+
+
+struct Node<G: Game> {
+    mov: G::Move,
+    parent: Option<Rc<Node<G>>>,
+    children: Vec<Node<G>>,
+    player_just_moved: G::Player,
+}
+
+impl<G: Game> Node<G> {
+    fn untried_moves(&self, legal_moves: G::MoveList) -> G::MoveList {
+        unimplemented!();
     }
+
+    fn select_child(&self, legal_moves: G::MoveList) -> Self {
+        unimplemented!();
+    }
+
+    fn add_child(&mut self) {
+        unimplemented!();
+    }
+
+    fn update(&mut self, result: f64) {
+        unimplemented!();
+    }
+}
+
+
+pub trait ISMCTS {
+
+    fn select(&self);
+
 }
